b6ff8293730b52b11e0122e3e9c50ae28f25d2ad
Felipe Catania <felicatania@gmail.com>
2017-11-29 12:15:30 -0800

    Implemented Queue for the Functional Pattern, passing all specs.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,30 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    for (var key in storage) {
+      var holder = storage[key];
+      delete storage[key];
+      someInstance.refactorStorage();
+      return holder;
+    }
   };
 
+  someInstance.refactorStorage = function() {
+    var newStorage = {};
+    var i = 0;
+    for (var key in storage) {
+      newStorage[i] = storage[key];
+      i++;
+    }
+    storage = newStorage;
+  }
+
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
