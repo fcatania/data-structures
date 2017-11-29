d85366e88994e3457e531ebf493234a3d3436773
fcatania <felicatania@gmail.com>
2017-11-29 15:52:58 -0800

    Implemented Stack() and Queue() for Pseudoclassical Pattern, passing all specs.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeTracker = 0;
+  this.count = 0;
+  this.lowestCount = 0;
 };
 
 
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+  this.sizeTracker++;
+};
+
+Queue.prototype.dequeue = function() {
+  var holder;
+  if (this.sizeTracker !== 0) {
+    holder = this.storage[this.lowestCount];
+    delete this.storage[this.lowestCount];
+    this.lowestCount++;
+    this.sizeTracker--;
+  }
+  return holder;
+};
+
+Queue.prototype.size = function() {
+  return this.sizeTracker;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeTracker = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.sizeTracker++;
+  this.storage[this.sizeTracker] = value;
+  
+};
 
+Stack.prototype.pop = function() {
+  var holder;
+  if (this.sizeTracker !== 0) {
+    var holder = this.storage[this.sizeTracker];
+    delete this.storage[this.sizeTracker];
+    this.sizeTracker--;
+  }
+  return holder;
+};
+
+Stack.prototype.size = function() {
+  return this.sizeTracker;
+};
