c3b4febfe83f3330d9abb88fc71c25b90c1bce2b
fcatania <felicatania@gmail.com>
2017-11-29 18:04:32 -0800

    Implemented Tree(), passing all specs.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,36 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  
+  extend(newTree, treeMethods);
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var contains = false;
+  var contain = function(tree, target) {
+    if (tree.value === target) {
+      contains = true;
+    } else {
+      for (var i = 0; i < tree.children.length; i++) {
+        contain(tree.children[i], target);
+      }
+    }
+  };
+  contain(this, target);
+  return contains;  
 };
 
 
