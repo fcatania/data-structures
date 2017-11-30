dfaaea530c3580c8e5511ff45987b95935cc229d
fcatania <felicatania@gmail.com>
2017-11-29 17:38:25 -0800

    Implemented LinkedList(), passing all specs.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,48 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (!list.head) {
+      list.tail = node;
+      list.head = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var holder;
+    var headNode = list.head;
+    if (headNode === null) {
+      return null;
+    } else if (headNode === list.tail) {
+      holder = headNode.value;
+      headNode = null;
+      list.tail = null;
+      return holder;
+    } else {
+      holder = headNode.value;
+      list.head = headNode.next;
+      return holder;
+    }
   };
 
   list.contains = function(target) {
+    var contain = function(node, target) {
+      if (node === null) {
+        return false;
+      } else if (node.value === target) {
+        return true;
+      } else {
+        return contain(node.next, target);
+      }
+    };
+    return contain(list.head, target);
   };
-
+  
   return list;
 };
 
@@ -23,6 +56,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
